d3133bcaa2068dd77358fc69052e55f75076cf07
Sven Knispel <sven.knispel@gmail.com>
2014-02-01 22:54:47 +0100

    added office365 powershell scripts

---
+++ office365/Office365_CreateSiteCollection.ps1
@@ -0,0 +1,78 @@
+﻿# by Sven Knispel under ter terms of the Ms-PL license (see license.txt)
+#
+# Creates a site collection on office365 using the office online commandlets
+#
+# History
+# 1.0 	2014-02-01 	Initial version 
+########################################################################
+
+$user         = "sven.knispel@svenknispel.onmicrosoft.com"
+$site         = "https://svenknispel-admin.sharepoint.com"
+$locale       = 1033  # english
+$newSite      = "https://svenknispel.sharepoint.com/sites/teamsitecollection1" 
+$newSiteTitle = "Created by PowerShell"
+            
+Import-Module -name "Microsoft.Online.SharePoint.PowerShell" -DisableNameChecking
+
+# connect
+    Write-Host "Connecting to service"
+try
+{
+    Connect-SPOService -Url $site -Credential $user
+
+    Write-Host -ForegroundColor Green "Connected"
+    
+    # check if site exists
+    $site = Get-SPOSite $newSite
+
+    if ($site -ne $null)
+    {
+
+        Write-Host -ForegroundColor Red "Site $newSite already exists"
+      
+        $a = new-object -comobject wscript.shell 
+
+        $choice = ""
+        while ($choice -notmatch "[y|n]")
+        {
+            $choice = read-host "Site collection $newSite already exists. Do you want to delete and re-create it? (Y/N)"
+        }
+
+        if ($choice -eq "y")
+        {
+            # delete and purge
+            Write-Host "Deleting $newSite"
+            Remove-SPOSite $newSite -Confirm:$false
+
+            Write-Host "Removing $newSite from recycle bin"
+            Remove-SPODeletedSite -Identity $newSite -Confirm:$false
+
+            # create a site collection
+            Write-Host "Creating $newSite"
+            New-SPOSite -Url $newSite -Owner $user -LocaleId $locale -StorageQuota 100 -ResourceQuota 1  -Title $newSiteTitle -Template STS#0 -NoWait
+
+        }
+        else
+        {
+            write-host "Doing nothing!"
+        }
+    }
+    else
+    {
+        # create a site collection
+        Write-Host "Creating $newSite"
+        New-SPOSite -Url $newSite -Owner $user -LocaleId $locale -StorageQuota 100 -ResourceQuota 1  -Title $newSiteTitle -Template STS#0 -NoWait
+
+    }
+
+    # disconnect
+    Disconnect-SPOService
+}
+catch 
+{
+    Write-Host -ForegroundColor Red "Unable to connect"
+}
+
+
+
+
---
+++ office365/Office365_CreateSites.ps1
@@ -0,0 +1,63 @@
+﻿# by Sven Knispel under ter terms of the Ms-PL license (see license.txt)
+#
+# Add a site to a site collection using CSOM
+#
+# History
+# 1.0 	2014-02-01 	Initial version 
+########################################################################
+
+$url      = "https://svenknispel.sharepoint.com/sites/teamsitecollection1" 
+$newSiteTitle = "Created by PowerShell"
+
+$username = Read-Host -Prompt "Enter username (user@domain.onmicrosoft.com)" 
+$securePassword = Read-Host -Prompt "Enter password" -AsSecureString
+ 
+#$securePassword = ConvertTo-SecureString $Password -AsPlainText -Force 
+ 
+# we are on a sharepoint 2013 server. Path may differ
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.dll" 
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.Runtime.dll" 
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.Taxonomy.dll" 
+ 
+# connect/authenticate to SharePoint Online and get ClientContext object.. 
+$clientContext = New-Object Microsoft.SharePoint.Client.ClientContext($url) 
+$credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $securePassword) 
+$clientContext.Credentials = $credentials 
+ 
+if (!$clientContext.ServerObjectIsNull.Value) 
+{ 
+    Write-Host "Connected to SharePoint Online site: '$Url'" -ForegroundColor Green 
+} 
+
+$rootWeb = $clientContext.Web
+$childWebs = $rootWeb.Webs
+$clientContext.Load($rootWeb)
+$clientContext.Load($childWebs)
+$clientContext.ExecuteQuery()
+ 
+function processWeb($web)
+{
+    $lists = $web.Lists
+    $clientContext.Load($web)
+    $clientContext.ExecuteQuery()
+    Write-Host "Web URL is" $web.Url
+}
+
+# create web 
+$wcInfo = New-Object Microsoft.SharePoint.Client.WebCreationInformation
+$wcInfo.Description = "This is a site create using powershell with CSOM"
+$wcInfo.Title = "another web"
+$wcInfo.UseSamePermissionsAsParentSite = $false
+$wcInfo.Url = "anotherweb2"
+$wcInfo.WebTemplate = "STS#0“
+$wcInfo.Language = 1033  # english
+
+$newWeb = $rootWeb.Webs.Add($wcInfo);
+$clientContext.Load($newWeb)
+$clientContext.ExecuteQuery()
+
+#list webs
+foreach ($childWeb in $childWebs)
+{
+    processWeb($childWeb)
+}
