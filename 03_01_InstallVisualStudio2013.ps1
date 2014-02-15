54277605ec0e27bfb854952aebb04894feb7956a
Sven Knispel <sven.knispel@gmail.com>
2014-02-15 11:04:28 +0100

    Fixed bug in path/args

--- 03_01_InstallVisualStudio2013.ps1
+++ 03_01_InstallVisualStudio2013.ps1
@@ -1,10 +1,11 @@
-# by Sven Knispel under ter terms of the Ms-PL license (see license.txt)
+ven Knispel under ter terms of the Ms-PL license (see license.txt)
 #
 # Installs Visual Studio 2013 ultimate (the product must later be activated with a proper key
 #
 # History
 # 1.0 	2014-01-03 	Initial version 
 # 1.1	2014-02-14	Externalized config 
+# 1.2   2014-02-15	Fixed bug in path/arguments
 ########################################################################
 Get-Content "config.txt" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
 
@@ -24,10 +25,9 @@ if (-Not(Test-Path $setupPath))
 }
 $psExecPath = Join-Path -Path $developerFolderPath "tools\PsExec.exe"
 $psExecLogPath = Join-Path -Path $developerFolderPath "tools\PsExecLog.txt"
+$configFilePath = Join-Path -Path $developerFolderPath "VisualStudioConfig.xml"
 
-$configFile = $psExecLogPath = Join-Path -Path $softwareSetupPath "VisualStudioConfig.xml"
-    
-$arguments = " /AdminFile $configFile"
+$arguments = " /AdminFile $configFilePath"
 
 $arguments = $setupPath + $arguments
 
