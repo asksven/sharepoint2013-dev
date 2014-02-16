bf11a9ff1e5fa81ea935766eb89c6c7aa866a5f3
Sven Knispel <sven.knispel@gmail.com>
2014-02-16 11:55:39 +0100

    Fixed #2

--- 03_03_CreateADAccounts.ps1
+++ 03_03_CreateADAccounts.ps1
@@ -5,6 +5,7 @@
 # History
 # 1.0 	2014-01-03 	Initial version 
 # 1.1	2014-02-14	Externalized config 
+# 1.2	2014-02-16	Make sure to add user with a never expiring password
 ########################################################################
 Get-Content "config.txt" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
 
@@ -28,5 +29,5 @@ foreach ($User in $Users)
     $SAM =  $User
     Write-Host "(VM) $(Get-Date): Creating user $SAM"
     Remove-ADUser -Identity $SAM
-    New-ADUser -Name $SAM -SamAccountName $SAM -UserPrincipalName $SAM -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -Enabled $true
-} 
+    New-ADUser -Name $SAM -SamAccountName $SAM -UserPrincipalName $SAM -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -Enabled $true -PasswordNeverExpires $true
+}  
