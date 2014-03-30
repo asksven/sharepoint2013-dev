070190e824ba34e30177e6dbe67d961c888b9ca6
Sven Knispel <sven.knispel@gmail.com>
2014-03-30 23:45:22 +0200

    modified:   02_04_TestSqlServerConnection.ps1 Make sure to use the alias and not the server name to test the connection
    new file:   AutoSPInstaller/AutoSPInstaller/AutoSPInstallerInput_BI.xml
    Added "heavyweight" configuration with BDC, Performance Point and other BI stuff

--- 02_04_TestSqlServerConnection.ps1
+++ 02_04_TestSqlServerConnection.ps1
@@ -8,7 +8,7 @@
 ########################################################################
 Get-Content "config.txt" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
 
-﻿$sqlServer = $h.computerName
+$sqlServer = $h.sqlAliasName #$h.computerName
 
 $objSQLConnection = New-Object System.Data.SqlClient.SqlConnection
 try
